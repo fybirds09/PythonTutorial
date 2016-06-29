0673159807d8974b6b16c23fae8a564d5bdab356
Xiaoyu Yao <Xiaoyu Yao>
2016-06-28 21:59:28 -0700

    Producer Consumer using sleep

---
+++ producer_consumer_sleep.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'ProducerConsumer using sleep'
+
+
+from threading import Thread, Lock
+import time
+import random
+
+que = []
+lock = Lock()
+
+class ProducerThread(Thread):
+    def run(self):
+        nums = range(5)
+        global que
+        while True:
+            num = random.choice(nums)
+            lock.acquire()
+            que.append(num)
+            print("Pruduced", num)
+            lock.release()
+            time.sleep(random.random()*2)
+
+
+class ConsumerThread(Thread):
+    def run(self):
+        global que
+        while True:
+            while not que:
+                print("Nothing in que")
+                time.sleep(random.random())
+            lock.acquire()
+            num = que.pop(0)
+            print("Consumed", num)
+            lock.release()
+            time.sleep(random.random())
+
+
+if __name__ == '__main__':
+    ProducerThread().start()
+    ConsumerThread().start()
